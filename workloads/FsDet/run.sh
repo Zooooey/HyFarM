dfedfd656377a2252efc4c697758c6ef8da61085
Jing Wang <37525339+wjing6@users.noreply.github.com>
2022-04-08 11:09:54 +0800

    Update run.sh

--- workloads/FsDet/run.sh
+++ workloads/FsDet/run.sh
@@ -2,3 +2,5 @@ conda activate fsdet
 cd /home/wjing/hfm-workloads/few-shot-object-detection
 
 python3 -m demo.demo --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_ft_all_1shot.yaml   --input ../input/bike.jpg --output output/ --opts MODEL.WEIGHTS fsdet://coco/tfa_cos_1shot/model_final.pth  MODEL.DEVICE cpu
+
+python3 -m demo.demo --config-file configs/COCO-detection/faster_rcnn_R_101_FPN_ft_all_1shot.yaml   --video-input ../input/test2.mp4 --output output/ --opts MODEL.WEIGHTS fsdet://coco/tfa_cos_1shot/model_final.pth  MODEL.DEVICE cpu
