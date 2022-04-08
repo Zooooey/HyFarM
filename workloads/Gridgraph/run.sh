954af02e997409eafef9821680b85f6b1ff46c92
Jing Wang <37525339+wjing6@users.noreply.github.com>
2022-04-08 11:08:54 +0800

    Create run.sh

---
+++ workloads/Gridgraph/run.sh
@@ -0,0 +1,5 @@
+cd  /home/wangjing/fargraph-client/test_gridgraph
+./bin/bfs aaaGrid4 0 1
+./bin/bfs ../dataset/twtest-8/ 0 1
+./bin/pagerank ../dataset/twtest-8/ 2
+./bin/pagerank ../dataset/twtest-8/ 20
